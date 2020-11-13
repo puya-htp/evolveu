49cdaeec5cc4f5eae0a012bc25df07407c9918e2
HmingPuia <sawmasn7@gmail.com>
2020-11-12 22:10:24 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const Joi=require('joi')
+const express=require('express');
+const app=express();
+app.use(express.json());
+
+const courses=[
+    { id:1, name:"James"},
+    { id:2, name:"Roger"},
+    { id:3, name:"Patrick"},
+    { id:4, name:"Puya"},
+]
+app.get('/',(req,res)=>{
+    res.send('Hello This is EvoilveU Project Catalog');
+});
+app.get('/api/courses',(req,res)=>{
+    
+    res.send(courses);
+})
+app.post('/api/courses',(req,res)=>{
+    const schema={
+        name: Joi.string().min(3).required()
+
+    }
+    const result=Joi.validate(req.body,schema);
+    if(result.error){
+        res.status(400).send(result.error);
+        return;
+    }
+    const course={
+        id:courses.length + 1,
+        name:req.body.name
+    }
+    courses.push(course);
+    res.send(course)
+
+})
+
+app.get('/api/courses/:id',(req,res)=>{
+    const course=courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course)res.status(404).send('This course with the given ID was not found');
+    res.send(course)
+})
+
+app.listen(5000,()=>{
+    console.log('Server running');
+})
